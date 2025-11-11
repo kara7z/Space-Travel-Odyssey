32b0a00551315500a45435e11a76c19406836a3f
kara7z <kara.ossama12@gmail.com>
2025-11-11 17:30:06 +0100

    Adding Profile Elements

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,13 +1,61 @@
+let userData = [];
+
+fetch('data/user.json')
+    .then(res => {
+        if (!res.ok) throw new Error('Failed to load user data');
+        return res.json();
+    })
+    .then(data => {
+        userData = data;
+        startLiveChecks();  
+    })
+    .catch(error => console.error('Error fetching user data:', error));
 
 
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const loginBtn = document.getElementById('loginBtn');
+const Hidden = (e) => e.style.display = 'none';
 
-fetch("data/user.json")
-    .then(response => response.json())
-    .then(values => values.forEach(value=>console.log(value.email)));
-   
+function LiveEventListener(Element, Func) {
+  Element.addEventListener('input', Func);
+}
+
+function checkInput(input, correctValue, feedbackId,isCorrectFlag) {
+
+    const feedback = document.getElementById(feedbackId);
+
+    return () => {
+        const typed = input.value.trim();
+        const correct = typed === correctValue;
 
 
-display("Hello",4);
-  display("Hello",5); 
- const display = (fa,four) => console.log(fa,four) 
-   
+        if (isCorrectFlag === 'email') isEmailCorrect = correct;
+        if (isCorrectFlag === 'password') isPasswordCorrect = correct;
+
+        if (typed === "") {
+            feedback.textContent = "";
+            feedback.style.color = "";
+        }
+        else if (typed === correctValue) {
+            feedback.textContent = "Correct";
+            feedback.style.color = "#65FF54";
+        }
+        else {
+            feedback.textContent = "You enter wrong information";
+            feedback.style.color = "#FF7154";
+        }
+        loginBtn.disabled = !(isEmailCorrect && isPasswordCorrect);
+    };
+}
+
+
+function startLiveChecks() {
+    if (userData.length === 0) return;
+
+    const correctEmail = userData[0].email;
+    const correctPassword = userData[0].password;
+
+    LiveEventListener(email, checkInput(email, correctEmail, 'email-feedback','email'));
+    LiveEventListener(password, checkInput(password, correctPassword, 'password-feedback','password'));
+}
